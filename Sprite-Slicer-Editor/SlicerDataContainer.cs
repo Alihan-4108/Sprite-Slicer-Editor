0214b8f108ab38a3d66e5ac06bd201b663fb0381
Alihan <74368418+Alihan-4108@users.noreply.github.com>
2026-05-11 11:13:07 +0300

    Delete Sprite-Slicer-Editor directory

--- Sprite-Slicer-Editor/SlicerDataContainer.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class SlicerDataContainer : ScriptableObject
-{
-    public List<Sprite> sprites = new List<Sprite>();
-}
